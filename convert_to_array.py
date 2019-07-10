31c7d5e202cbb855cbcd5ac7b7985f2aa76c17a6
sahanant <sahanant27@gmail.com>
2019-07-10 15:52:36 +0530

    Updated Convert to array

--- convert_to_array.py
+++ convert_to_array.py
@@ -1,12 +1,19 @@
 import numpy as np
 import os 
 import argparse
+import cv2
+#import matplotlib.pyplot as plt
+
 
 #creating arguments
 parser = argparse.ArgumentParser(description = 'image_to_array')
 parser.add_argument('-l','--frame_location',type = str, metavar = '', help = 'location of animations' )
 parser.add_argument('-a','--array_location',type = str, metavar = '', help = 'location of arrays' )
 args = parser.parse_args()
+
+
+
+training_data=[]
 
 #check if animations exist
 def start(frame_location,array_location):
@@ -17,11 +24,22 @@ def start(frame_location,array_location):
 
 #convert to array
 def convert(frame_location,array_location):
-    dirs = os.listdir(frame_location)
-    for file in dirs:
-       temp = np.asarray(file)
-       np.savez_compressed(array_location,temp,'arrays') 
+    for path in os.listdir(frame_location):
+            img=cv2.imread(frame_location, cv2.IMREAD_UNCHANGED)
+            training_data.append(img)
+    path2=os.path.join(array_location,"training_data.npy")
+    np.save(path2, training_data)
 
 
 if __name__ == '__main__':
     start(args.frame_location,args.array_location)
+
+os.chdir(r"E:\projects\video")
+
+path_=r"E:\projects\video\animation"
+
+
+    
+
+
+
---
+++ pilot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 10:34:53 2019
+
+@author: Anant
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+c=np.load("array_dir.npz")
